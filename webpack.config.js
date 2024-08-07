acf8be096cf6f1c4c15c754abf5806a8eb783594
shahilpateliflair <shahil.patel@iflair.com>
2024-08-07 15:09:52 +0530

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
